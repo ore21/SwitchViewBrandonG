eabe57e38d36ed422714bfd9220d63e21956da39
ore21 <bgonzalez526@students.mchenry.edu>
2019-02-20 17:23:24 -0600

    Initial Commit

---
+++ SwitchView/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SwitchView
+//
+//  Created by student on 2/20/19.
+//  Copyright © 2019 student. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
